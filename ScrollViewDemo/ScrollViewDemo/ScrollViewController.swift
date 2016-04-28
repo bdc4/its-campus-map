87eb1443ccc966a5929dfeb65743c9c8b6cd9baf
student <student@NetBoot005.local>
2016-04-28 10:30:10 -0700

    Started playing with the setupGestureRecognizer to recognize a single tap and to not eat it

--- ScrollViewDemo/ScrollViewDemo/ScrollViewController.swift
+++ ScrollViewDemo/ScrollViewDemo/ScrollViewController.swift
@@ -46,10 +46,13 @@ class ScrollViewController: UIViewController, UIScrollViewDelegate {
         let imageViewSize = imageView.frame.size
         let scrollViewSize = scrollView.bounds.size
         
-        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
-        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageViewSize.width) / 2 : 0
         
-        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding,
+                                               bottom: verticalPadding, right: horizontalPadding)
     }
     
     func setZoomScale() {
@@ -66,9 +69,20 @@ class ScrollViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func setupGestureRecognizer() {
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(ScrollViewController.handleDoubleTap(_:)))
+        let doubleTap = UITapGestureRecognizer(target: self,
+                                               action: #selector(ScrollViewController.handleDoubleTap(_:)))
         doubleTap.numberOfTapsRequired = 2
+        doubleTap.cancelsTouchesInView = true
         scrollView.addGestureRecognizer(doubleTap)
+        
+        let singleTap = UITapGestureRecognizer(target: self,
+                                               action: #selector(ScrollViewController.handleDoubleTap(_:)))
+        singleTap.numberOfTapsRequired = 1
+        singleTap.cancelsTouchesInView = false
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        
     }
     
     func handleDoubleTap(recognizer: UITapGestureRecognizer) {
