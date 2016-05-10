ce4f087b4025a7d6c3570561282b0f4ecee60521
jogden95 <jennogden95@yahoo.com>
2016-05-10 11:17:55 -0700

    Add files via upload

---
+++ ScrollViewDemo-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+<<<<<<< HEAD
+//  ScrollViewDemo-Bridging-Header.h
+//  ScrollViewDemo
+//
+//  Created by jennifer ogden on 5/10/16.
+//  Copyright © 2016 Appcoda. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
+=======
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "SWRevealViewController.h"
+>>>>>>> 58f636869600ce74f16c3cf1f3a3d56b630ef3c5
